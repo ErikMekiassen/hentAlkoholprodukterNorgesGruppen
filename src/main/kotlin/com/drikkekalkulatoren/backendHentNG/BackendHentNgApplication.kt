d2511bbec70c8de1bffb6daa25725436586260fb
Emvit <81639393+ErikMekiassen@users.noreply.github.com>
2023-04-19 09:33:05 +0200

    Initial commit

---
+++ src/main/kotlin/com/drikkekalkulatoren/backendHentNG/BackendHentNgApplication.kt
@@ -0,0 +1,11 @@
+package com.drikkekalkulatoren.backendHentNG
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendHentNgApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendHentNgApplication>(*args)
+}
